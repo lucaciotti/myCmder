3948708cd5353fc419f7c5e78ef6ff3b61cda8d7
Luca <luca.ciotti@gmail.com>
2017-11-03 13:01:27 +0100

    Modifica funzionale per lasciare ssh-agent attivo all'uscita di Cmder. Devo riuscire ad inserire tutto in un unico batch...

--- bin/my-ssh-agent.cmd
+++
@@ -1,9 +0,0 @@
-call start-ssh-agent
-
-@del C:\tools\cmder\bin\ssh-agent-setter.bat
-
-@echo @SETX /m SSH_AGENT_PID %SSH_AGENT_PID%>C:\tools\cmder\bin\ssh-agent-setter.bat
-@echo @SETX /m SSH_AUTH_SOCK %SSH_AUTH_SOCK%>>C:\tools\cmder\bin\ssh-agent-setter.bat
-
-@start  C:\tools\cmder\bin\my-ssh-agent-setter.bat
-	  
---
+++ bin/ssh-agent-setter.bat
@@ -0,0 +1,2 @@
+@SETX /m SSH_AGENT_PID 2364
+@SETX /m SSH_AUTH_SOCK /tmp/ssh-wn8kVve4Mmpv/agent.6520
